0ee405a84c4c98a4aa080f233553b0c8e9a14d0f
Brandon Donnelson <branflake2267@gmail.com>
2012-12-10 12:49:26 -0800

    Tuning the pom and adding a script to run the commands

---
+++ create_archetype.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# http://www.automatedbusinesslogic.com/articles/deploying-a-maven-archetype
+
+echo "Started"
+
+CURRENTDIR=`pwd`
+
+# move to the archetype
+cd ./archetype-gwtp-basic-appengine
+
+# clean house
+mvn clean
+mvn idea:clean
+mvn eclipse:clean
+rm -R .settings
+rm -R bin
+
+echo "mvn archetype:create-from-project"
+mvn archetype:create-from-project
+
+cd target/generated-sources/archetype/
+mvn install
+
+cd $CURRENTDIR
+
+# TODO upload to sonatype
+#mvn archetype:crawl -Dcatalog=$CURRENTDIR/archetype-catalog.xml
+
+echo "Finished"
+
+echo "\n**Run this new project directory:**\nmvn archetype:generate -DarchetypeCatalog=local\n"
