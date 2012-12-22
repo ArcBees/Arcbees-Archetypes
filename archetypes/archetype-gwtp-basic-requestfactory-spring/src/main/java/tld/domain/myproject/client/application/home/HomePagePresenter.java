dc23cae2e6b23e4a73f1638acbcd5d21c031146c
Brandon Donnelson <branflake2267@gmail.com>
2012-12-22 13:27:35 -0800

    Domain Refactor Mrabti's archetype

---
+++ archetypes/archetype-gwtp-basic-requestfactory-spring/src/main/java/tld/domain/myproject/client/application/home/HomePagePresenter.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2012 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tld.domain.myproject.client.application.home;
+
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.requestfactory.shared.Receiver;
+import com.gwtplatform.mvp.client.HasUiHandlers;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+
+import java.util.List;
+
+import tld.domain.myproject.client.application.ApplicationPresenter;
+import tld.domain.myproject.client.place.NameTokens;
+import tld.domain.myproject.client.request.MyRequestFactory;
+import tld.domain.myproject.client.request.MyServiceRequest;
+import tld.domain.myproject.client.request.proxy.MyEntityProxy;
+
+public class HomePagePresenter extends Presenter<HomePagePresenter.MyView, HomePagePresenter.MyProxy>
+        implements HomeUiHandlers {
+    public interface MyView extends View, HasUiHandlers<HomeUiHandlers> {
+        void editUser(MyEntityProxy myEntity);
+
+        void setData(List<MyEntityProxy> data);
+    }
+
+    @ProxyStandard
+    @NameToken(NameTokens.home)
+    public interface MyProxy extends ProxyPlace<HomePagePresenter> {
+    }
+
+    private final MyRequestFactory requestFactory;
+
+    private MyServiceRequest currentContext;
+    private String searchToken;
+
+    @Inject
+    public HomePagePresenter(final EventBus eventBus, final MyView view, final MyProxy proxy,
+                             final MyRequestFactory requestFactory) {
+        super(eventBus, view, proxy, ApplicationPresenter.TYPE_SetMainContent);
+
+        this.requestFactory = requestFactory;
+
+        getView().setUiHandlers(this);
+    }
+
+    @Override
+    public void saveEntity(MyEntityProxy myEntity) {
+        currentContext.create(myEntity).fire(new Receiver<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                loadEntities();
+                initializeContext();
+            }
+        });
+    }
+
+    @Override
+    protected void onReveal() {
+        searchToken = "";
+        initializeContext();
+        loadEntities();
+    }
+
+    private void initializeContext() {
+        currentContext = requestFactory.myService();
+        MyEntityProxy newEntity = currentContext.create(MyEntityProxy.class);
+        getView().editUser(newEntity);
+    }
+
+    private void loadEntities() {
+        requestFactory.myService().loadAll(searchToken).fire(new Receiver<List<MyEntityProxy>>() {
+            @Override
+            public void onSuccess(List<MyEntityProxy> data) {
+                getView().setData(data);
+            }
+        });
+    }
+}
---
+++ archetypes/archetype-gwtp-basic-requestfactory-spring/src/main/java/tld/domain/myproject/client/application/home/HomePageView.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2012 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tld.domain.myproject.client.application.home;
+
+import com.arcbees.core.client.mvp.ViewWithUiHandlers;
+import com.arcbees.core.client.mvp.uihandlers.UiHandlersStrategy;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.inject.Inject;
+
+import java.util.List;
+
+import tld.domain.myproject.client.application.home.ui.MyEntityEditor;
+import tld.domain.myproject.client.request.proxy.MyEntityProxy;
+
+public class HomePageView extends ViewWithUiHandlers<HomeUiHandlers> implements HomePagePresenter.MyView {
+    public interface Binder extends UiBinder<Widget, HomePageView> {
+    }
+
+    @UiField(provided = true)
+    MyEntityEditor myEntityEditor;
+    @UiField(provided = true)
+    CellTable<MyEntityProxy> myTable;
+
+    private final ListDataProvider<MyEntityProxy> dataProvider;
+
+    @Inject
+    public HomePageView(final Binder uiBinder, final MyEntityEditor myEntityEditor,
+                        final ListDataProvider<MyEntityProxy> dataProvider,
+                        final UiHandlersStrategy<HomeUiHandlers> uiHandlers) {
+        super(uiHandlers);
+
+        this.myEntityEditor = myEntityEditor;
+        this.dataProvider = dataProvider;
+        this.myTable = new CellTable<MyEntityProxy>();
+
+        initWidget(uiBinder.createAndBindUi(this));
+        initCellTable();
+        dataProvider.addDataDisplay(myTable);
+    }
+
+    @Override
+    public void editUser(MyEntityProxy myEntity) {
+        myEntityEditor.edit(myEntity);
+    }
+
+    @Override
+    public void setData(List<MyEntityProxy> data) {
+        dataProvider.getList().clear();
+        dataProvider.getList().addAll(data);
+        dataProvider.refresh();
+    }
+
+    @UiHandler("submit")
+    void onSubmitClicked(ClickEvent event) {
+        getUiHandlers().saveEntity(myEntityEditor.get());
+    }
+
+    private void initCellTable() {
+        TextColumn<MyEntityProxy> firstNameColumn = new TextColumn<MyEntityProxy>() {
+            @Override
+            public String getValue(MyEntityProxy object) {
+                return object.getFirstName();
+            }
+        };
+        myTable.addColumn(firstNameColumn, "First name");
+
+        TextColumn<MyEntityProxy> lastNameColumn = new TextColumn<MyEntityProxy>() {
+            @Override
+            public String getValue(MyEntityProxy object) {
+                return object.getLastName();
+            }
+        };
+        myTable.addColumn(lastNameColumn, "Last name");
+    }
+}
