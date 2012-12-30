b1f4c4d2d47a2da3bd8b86c279bb18b4dd86efcc
Brandon Donnelson <branflake2267@gmail.com>
2012-12-30 08:59:54 -0800

    Upgrading to auto ginjector generation

--- archetypes/gwtp-appengine-objectify/src/main/java/com/arcbees/tasks/client/MyProject.java
+++ archetypes/gwtp-appengine-objectify/src/main/java/com/arcbees/tasks/client/MyProject.java
@@ -16,21 +16,15 @@
 
 package com.arcbees.tasks.client;
 
-
-import com.arcbees.tasks.client.gin.ClientGinjector;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.gwtplatform.mvp.client.DelayedBindRegistry;
+import com.gwtplatform.mvp.client.ApplicationController;
 
 public class MyProject implements EntryPoint {
-    private final ClientGinjector ginjector = GWT.create(ClientGinjector.class);
+    public final ApplicationController controller = GWT.create(ApplicationController.class);
 
     @Override
     public void onModuleLoad() {
-        // This is required for Gwt-Platform proxy's generator
-        DelayedBindRegistry.bind(ginjector);
-
-        // process current user first
-        ginjector.getBootStrapper().init();
+      controller.init();
     }
 }
