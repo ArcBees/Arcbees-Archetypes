ba0015b758739070891217fc1f61debad3c9ed5e
Brandon Donnelson <branflake2267@gmail.com>
2013-01-06 09:22:11 -0800

    refactor a more generic project package name

---
+++ archetypes/gwtp-appengine-objectify/src/main/java/com/arcbees/project/server/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.arcbees.project.server.dao;
+
+import com.arcbees.project.shared.domain.User;
+
+public class UserDao extends BaseDao<User> {
+    public UserDao() {
+        super(User.class);
+    }
+
+    public User findByGoogleId(String googleId) {
+        return ofy().query(User.class).filter("googleId =", googleId).first().get();
+    }
+}
