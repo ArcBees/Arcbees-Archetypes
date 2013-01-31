9a81452002045244d86c237c4bcc89aaaeeda81c
Brandon Donnelson <branflake2267@gmail.com>
2013-01-31 13:36:59 -0800

    Formatting and TODOS

--- archetypes/gwtp-appengine-basic/src/main/java/com/arcbees/project/client/gin/ClientModule.java
+++ archetypes/gwtp-appengine-basic/src/main/java/com/arcbees/project/client/gin/ClientModule.java
@@ -19,21 +19,20 @@ package com.arcbees.project.client.gin;
 import com.arcbees.project.client.application.ApplicationModule;
 import com.arcbees.project.client.place.NameTokens;
 import com.gwtplatform.mvp.client.annotations.DefaultPlace;
-import com.gwtplatform.mvp.client.annotations.ErrorPlace;
-import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
 
 /**
  * See more on setting up the PlaceManager on <a
- * href="// See more on: https://github.com/ArcBees/GWTP/wiki/PlaceManager">DefaultModule's > DefaultPlaceManager</a>
+ * href="// See more on: https://github.com/ArcBees/GWTP/wiki/PlaceManager"
+ * >DefaultModule's > DefaultPlaceManager</a>
  */
 public class ClientModule extends AbstractPresenterModule {
-  @Override
-  protected void configure() {
-    install(new DefaultModule());
-    install(new ApplicationModule());
+    @Override
+    protected void configure() {
+        install(new DefaultModule());
+        install(new ApplicationModule());
 
-    bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.home);
-  }
+        bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.home);
+    }
 }
