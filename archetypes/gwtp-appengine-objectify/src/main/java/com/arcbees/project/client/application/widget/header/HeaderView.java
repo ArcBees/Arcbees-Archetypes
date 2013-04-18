824153aa3ddb6d1121d871856e5566e36aac1513
Brandon Donnelson <branflake2267@gmail.com>
2013-04-17 19:12:26 -0700

    Updated poms, iswidget, misc, RC-3 update.

--- archetypes/gwtp-appengine-objectify/src/main/java/com/arcbees/project/client/application/widget/header/HeaderView.java
+++ archetypes/gwtp-appengine-objectify/src/main/java/com/arcbees/project/client/application/widget/header/HeaderView.java
@@ -21,8 +21,8 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 
@@ -39,7 +39,7 @@ public class HeaderView extends ViewWithUiHandlers<HeaderUiHandlers> implements 
     }
     
     @Override
-    public void setInSlot(Object slot, Widget content) {
+    public void setInSlot(Object slot, IsWidget content) {
         if (slot == HeaderPresenter.TYPE_LoginPresenter) {
             login.setWidget(content);
         }
