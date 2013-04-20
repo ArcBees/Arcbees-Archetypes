7dce899aed8aafee3193fd7d372c703335ff0297
Brandon Donnelson <branflake2267@gmail.com>
2013-04-20 12:30:50 -0700

    Adding GXT contents

--- archetypes/gwtp-basic-gxt/src/main/java/com/arcbees/project/client/application/ApplicationView.java
+++ archetypes/gwtp-basic-gxt/src/main/java/com/arcbees/project/client/application/ApplicationView.java
@@ -21,35 +21,38 @@ import javax.inject.Inject;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import com.sencha.gxt.widget.core.client.container.Viewport;
 
+/**
+ * See more on GXT <a href="http://docs.sencha.com/gxt-guides/3/ui/layout/LayoutContainers.html">Layout Containers</a>
+ */
 public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
-    public interface Binder extends UiBinder<Widget, ApplicationView> {
-    }
+  public interface Binder extends UiBinder<Widget, ApplicationView> {
+  }
 
-    @UiField
-    SimplePanel main;
-    
-    private Widget widget;
+  @UiField
+  Viewport main;
 
-    @Inject
-    public ApplicationView(final Binder uiBinder) {
-        widget = uiBinder.createAndBindUi(this);
-    }
+  private Widget widget;
 
-    @Override
-    public void setInSlot(Object slot, IsWidget content) {
-        if (slot == ApplicationPresenter.TYPE_SetMainContent) {
-            main.setWidget(content);
-        } else {
-            super.setInSlot(slot, content);
-        }
-    }
+  @Inject
+  public ApplicationView(final Binder uiBinder) {
+    widget = uiBinder.createAndBindUi(this);
+  }
 
-    @Override
-    public Widget asWidget() {
-        return widget;
+  @Override
+  public void setInSlot(Object slot, IsWidget content) {
+    if (slot == ApplicationPresenter.TYPE_SetMainContent) {
+      main.setWidget(content);
+    } else {
+      super.setInSlot(slot, content);
     }
+  }
+
+  @Override
+  public Widget asWidget() {
+    return widget;
+  }
 }
--- archetypes/gwtp-basic-gxt/src/main/java/com/arcbees/project/client/application/home/HomePageView.java
+++ archetypes/gwtp-basic-gxt/src/main/java/com/arcbees/project/client/application/home/HomePageView.java
@@ -21,12 +21,15 @@ import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
 
+/**
+ * See more on GXT <a href="http://docs.sencha.com/gxt-guides/3/ui/layout/LayoutContainers.html">Layout Containers</a>
+ */
 public class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
-    public interface Binder extends UiBinder<Widget, HomePageView> {
-    }
+  public interface Binder extends UiBinder<Widget, HomePageView> {
+  }
 
-    @Inject
-    public HomePageView(final Binder uiBinder) {
-        initWidget(uiBinder.createAndBindUi(this));
-    }
+  @Inject
+  public HomePageView(final Binder uiBinder) {
+    initWidget(uiBinder.createAndBindUi(this));
+  }
 }
