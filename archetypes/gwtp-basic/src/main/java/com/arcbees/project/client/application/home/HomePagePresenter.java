8de9080b65713be616e8545226dcc55cfdf7dbb1
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-04-17 20:32:12 -0400

    Update Basic archetype

--- archetypes/gwtp-basic/src/main/java/com/arcbees/project/client/application/home/HomePagePresenter.java
+++ archetypes/gwtp-basic/src/main/java/com/arcbees/project/client/application/home/HomePagePresenter.java
@@ -16,9 +16,10 @@
 
 package com.arcbees.project.client.application.home;
 
+import javax.inject.Inject;
+
 import com.arcbees.project.client.application.ApplicationPresenter;
 import com.arcbees.project.client.place.NameTokens;
-import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
