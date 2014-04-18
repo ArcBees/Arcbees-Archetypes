5b87ba3eb8d7a6e8b1a5218ba2b209d8daab3a5e
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-04-17 20:55:28 -0400

    Update Plugin2 archetype

--- archetypes/gwtp-plugin2-basic/src/main/java/com/arcbees/project/server/guice/DispatchServletModule.java
+++ archetypes/gwtp-plugin2-basic/src/main/java/com/arcbees/project/server/guice/DispatchServletModule.java
@@ -17,8 +17,8 @@
 package com.arcbees.project.server.guice;
 
 import com.google.inject.servlet.ServletModule;
-import com.gwtplatform.dispatch.server.guice.DispatchServiceImpl;
-import com.gwtplatform.dispatch.shared.ActionImpl;
+import com.gwtplatform.dispatch.rpc.server.guice.DispatchServiceImpl;
+import com.gwtplatform.dispatch.rpc.shared.ActionImpl;
 
 public class DispatchServletModule extends ServletModule {
     @Override
