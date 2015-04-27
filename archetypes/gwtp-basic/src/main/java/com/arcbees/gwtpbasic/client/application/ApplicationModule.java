8e784bd5c43ec23ae0664c088e959a5436200d7b
jasonlemay <jason.lemay@arcbees.com>
2015-04-27 15:45:58 -0400

    Update GWTP Basic Archetype

--- archetypes/gwtp-basic/src/main/java/com/arcbees/gwtpbasic/client/application/ApplicationModule.java
+++ archetypes/gwtp-basic/src/main/java/com/arcbees/gwtpbasic/client/application/ApplicationModule.java
@@ -23,7 +23,7 @@ public class ApplicationModule extends AbstractPresenterModule {
     @Override
     protected void configure() {
         install(new HomeModule());
-        
+
         bindPresenter(ApplicationPresenter.class, ApplicationPresenter.MyView.class, ApplicationView.class,
                 ApplicationPresenter.MyProxy.class);
     }
