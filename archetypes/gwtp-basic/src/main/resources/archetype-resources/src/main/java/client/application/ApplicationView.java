c0294cdbb44f2c2311d43437c62c5a6aaa2dc229
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-18 11:15:12 -0400

    Rm new lines

--- archetypes/gwtp-basic/src/main/resources/archetype-resources/src/main/java/client/application/ApplicationView.java
+++ archetypes/gwtp-basic/src/main/resources/archetype-resources/src/main/java/client/application/ApplicationView.java
@@ -21,7 +21,4 @@ public class ApplicationView extends ViewImpl implements ApplicationPresenter.My
         initWidget(uiBinder.createAndBindUi(this));
 
         bindSlot(ApplicationPresenter.SLOT_MAIN, main);
-    }
-
-
 }
